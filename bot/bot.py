9046919ed6a82bbcb73166bcd8880caaa0c0f163
marco.almeida <otavio_marco@outlook.com>
2019-12-04 13:01:39 -0400

    bot: modularização do predcit e fit dialogo: alteacao da saudacao para pedido main: construção das funções de intenções

--- bot/bot.py
+++ bot/bot.py
@@ -1,59 +1,66 @@
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
-# from sklearn.tree import DecisionTreeClassifier
-# from sklearn.neighbors import KNeighborsClassifier
-# from sklearn.model_selection import LeaveOneOut
-# from sklearn.linear_model import LogisticRegression
-# from sklearn.metrics import accuracy_score
-# from sklearn.metrics import precision_score
-# from sklearn.metrics import recall_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import LeaveOneOut
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
 
 
 class Bot:
     def __init__(self, arquivo, classificador = 'knn'):
-        print('criado')
-    #     self.dialogos = list()
-    #     self.intencoes = list()
-    #     self.classificador = classificador.lower()
-    #     self.fit(arquivo)
-    #
-    # def fit(self, arquivo):
-    #     if arquivo is None:
-    #         with open(arquivo, 'r', encoding = 'utf-8-sig') as file:
-    #             for line in file:
-    #                 s = line.replace("\n", "").split('-')
-    #                 self.intencoes.append(s[0])
-    #                 self.dialogos.append(s[1])
-    #
-    #         vectorizer = TfidfVectorizer(sublinear_tf = True, max_df = 0.5, strip_accents = 'unicode')
-    #         y = np.array(self.intencoes)
-    #         x = vectorizer.fit_transform(self.dialogos)
-    #
-    #         y_true = list()
-    #         predict = list()
-    #
-    #         if self.classificador in 'tree':
-    #             classificador = DecisionTreeClassifier(random_state = 2)
-    #         elif self.classificador in 'logre':
-    #             classificador = LogisticRegression(random_state = 0, solver = 'lbfgs', multi_class = 'auto')
-    #         else:
-    #             if self.classificador not in 'knn':
-    #                 print('Classificador não mapeado. Utlizando KNN')
-    #
-    #             classificador = KNeighborsClassifier(n_neighbors = 2)
-    #
-    #         loo = LeaveOneOut()
-    #         loo.get_n_splits(x)
-    #
-    #         for train_index, test_index in loo.split(x):
-    #             x_train, x_test = x[train_index], x[test_index]
-    #             y_train, y_test = y[train_index], y[test_index]
-    #             y_true.append(y_test)
-    #
-    #             classificador.fit(x_train, y_train)
-    #             predict.append(classificador.predict(x_test))
-    #
-    #         print('== {}'.format(classificador))
-    #         print('\t= Acuracy: {}'.format(accuracy_score(y_true, predict)))
-    #         print('\t= Presision: {}'.format(precision_score(y_true, predict, average = 'macro')))
-    #         print('\t= Recall: {}'.format(recall_score(y_true, predict, average = 'macro')))
+        self.dialogos = list()
+        self.intencoes = list()
+        self.classificador = self.setClassificador(classificador)
+        self.vectorizer = TfidfVectorizer(sublinear_tf = True, max_df = 0.5, strip_accents = 'unicode')
+
+        self.fit(arquivo)
+
+    def setClassificador(self, x):
+        if x in 'tree':
+            classificador = DecisionTreeClassifier(random_state = 2)
+
+        elif x in 'logre':
+            classificador = LogisticRegression(random_state = 0, solver = 'lbfgs', multi_class = 'auto')
+
+        else:
+            if x not in 'knn':
+                print('Classificador não mapeado. Utlizando KNN')
+
+            classificador = KNeighborsClassifier(n_neighbors = 2)
+
+        return classificador
+
+    def predict(self, texto):
+        return self.classificador.predict(self.vectorizer.transform([texto]))
+
+    def fit(self, arquivo):
+        with open(arquivo, 'r', encoding = 'utf-8-sig') as file:
+            for line in file:
+                s = line.replace("\n", "").split('-')
+                self.intencoes.append(s[0])
+                self.dialogos.append(s[1])
+
+        y = np.array(self.intencoes)
+        x = self.vectorizer.fit_transform(self.dialogos)
+
+        y_true = list()
+        predict = list()
+
+        loo = LeaveOneOut()
+        loo.get_n_splits(x)
+
+        for train_index, test_index in loo.split(x):
+            x_train, x_test = x[train_index], x[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+            y_true.append(y_test)
+
+            self.classificador.fit(x_train, y_train)
+            predict.append(self.classificador.predict(x_test))
+
+        # print('== {}'.format(self.classificador))
+        # print('\t= Acuracy: {}'.format(accuracy_score(y_true, predict)))
+        # print('\t= Presision: {}'.format(precision_score(y_true, predict, average = 'macro')))
+        # print('\t= Recall: {}'.format(recall_score(y_true, predict, average = 'macro')))
--- main/main.py
+++ main/main.py
@@ -2,9 +2,37 @@ __author__ = 'Marco Almeida and André Furlan'
 
 from bot.bot import Bot
 
+def somaConta(conta):
+    #TODO
+    pass
+
+def exibePreco():
+    with open('../files/precos.csv', 'r', encoding = 'utf-8-sig') as file:
+        for line in file:
+            print(line.replace("\n", ""))
+
 def main():
-    bot = Bot('./files/dialogo.csv')
+    bot = Bot('../files/dialogo.csv')
+
+    print('Bem vindo a porra toda')
+
+    while True:
+        conta = list()
+        print('O que deseja?')
+        int = bot.predict(input())[0]
+
+        if int in 'pedido':
+            #TODO
+            pass
+        elif int in 'conta':
+            somaConta(conta)
+            pass
+        elif int in 'tabela':
+            exibePreco()
+            pass
+        elif int in 'saida':
+            #TODO
+            pass
 
 if __name__ == '__main__':
     main()
-
