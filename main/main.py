6c6c3dd41a308a8f72fa217859907e9cb5ecd16d
André Furlan <furlandk@gmail.com>
2019-12-06 00:00:48 -0400

    incluido respostas

--- main/main.py
+++ main/main.py
@@ -1,10 +1,38 @@
 __author__ = 'Marco Almeida and André Furlan'
 
 from bot.bot import Bot
+import random
 
 produtos = dict()
 carrinho = list()
 numeros = dict()
+frases_saudacao = list()
+frases_tabela = list()
+frases_pedido = list()
+frases_conta = list()
+frases_confirmacao = list()
+frases_negacao = list()
+frases_agradecimento = list()
+
+def constroiRespostas():
+    with open('../files/respostas.csv', 'r', encoding = 'utf-8-sig') as file:
+        for line in file:
+            intencao = line.replace('\n', '').split(';')[0]
+            frase = line.replace('\n', '').split(';')[1]
+            if intencao == 'saudacao':
+                frases_saudacao.append(frase)
+            elif intencao == 'tabela':
+                frases_tabela.append(frase)
+            elif intencao == 'pedido':
+                frases_pedido.append(frase)
+            elif intencao == 'conta':
+                frases_conta.append(frase)
+            elif intencao == 'confirmacao':
+                frases_confirmacao.append(frase)
+            elif intencao == 'negacao':
+                frases_negacao.append(frase)
+            elif intencao == 'agradecimento':
+                frases_agradecimento.append(frase)
 
 def constroiNumeros():
     with open('../files/numeros.csv', mode = 'r', encoding = 'utf-8-sig') as file:
@@ -12,13 +40,12 @@ def constroiNumeros():
             str, num = line.split(';')
             numeros[str] = num
 
-
 def somaConta(conta):
     #TODO
     pass
 
 def exibeProdutos():
-    print('\nOs produtos que temos hoje são')
+    print('\n' + random.choice(frases_saudacao))
     for key, value in produtos.items():
         print('{} -> R${}'.format(key, value).replace('\n', ''))
 
@@ -27,6 +54,8 @@ def cardapio():
         for line in file:
             produto, valor = line.split(';')
             produtos[produto] = valor
+
+
 
 def adicionaCarrinho(produto, quantidade):
     encontrou = False
@@ -41,12 +70,14 @@ def adicionaCarrinho(produto, quantidade):
         p = {'{}:{}'.format(produto, quantidade)}
         carrinho.append(p)
 
+    print(random.choice(frases_pedido))
+
 def saida():
     if len(produtos) > 0:
         somaConta(carrinho)
         print('sua conta total eh de xxxx')
 
-    print('Agradecemos sua visita!')
+    print('\n'+ random.choice(frases_agradecimento))
 
     return False
 
@@ -77,8 +108,10 @@ def main():
     bot = Bot('../files/dialogo.csv')
     constroiNumeros()
     cardapio()
+    constroiRespostas()
+
     print('{h} Bem vindo a loja {h}\n'.format(h = '#'* 8))
-    print('Em que posso ajudar?')
+    print(random.choice(frases_saudacao))
 
     loop = True
 
@@ -88,7 +121,10 @@ def main():
 
         print('Intencao {}'.format(int))
 
-        if int in 'pedido':
+        if int in 'saudacao':
+            print(random.choice(frases_saudacao))
+
+        elif int in 'pedido':
             verificarPedido(entrada)
             print(carrinho)
 
@@ -101,7 +137,8 @@ def main():
         elif int in 'saida':
             loop = saida()
 
-        if loop is True: print('Deseja algo mais?')
+        if loop is True:
+            print('Deseja algo mais?')
 
 if __name__ == '__main__':
     main()
