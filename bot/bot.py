c8be09dd7e00c22e33f57dedc45225fa740ed132
marco.almeida <otavio_marco@outlook.com>
2019-12-07 21:04:57 -0400

    Alteração das funções para o bot Diversas outras funções

--- bot/bot.py
+++ bot/bot.py
@@ -1,22 +1,188 @@
+from random import choice
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import LeaveOneOut
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
+# from sklearn.metrics import accuracy_score
+# from sklearn.metrics import precision_score
+# from sklearn.metrics import recall_score
 
 
 class Bot:
     def __init__(self, arquivo, classificador = 'knn'):
         self.dialogos = list()
         self.intencoes = list()
+        self.cart = list()
+        self.listaProdutos = self.buildProductList()
+        self.listaNumeros = self.buildNumberList()
+        self.listaPacotes = self.buildPackages()
+        self.listaProdutosPreco = self.buildProductsPrice()
+        self.listaRespostas = self.buildAnswers()
         self.classificador = self.setClassificador(classificador)
         self.vectorizer = TfidfVectorizer(sublinear_tf = True, max_df = 0.5, strip_accents = 'unicode')
 
         self.fit(arquivo)
+
+    def buildAnswers(self):
+        saudacao = list()
+        tabela = list()
+        pedido = list()
+        conta = list()
+        confirmacao = list()
+        negacao = list()
+        agradecimento = list()
+        continuidade = list()
+
+        with open('../files/respostas.csv', 'r', encoding = 'utf-8-sig') as file:
+            for line in file:
+                intencao = line.replace('\n', '').split(';')[0]
+                frase = line.replace('\n', '').split(';')[1]
+
+                if intencao in 'saudacao':
+                    saudacao.append(frase)
+
+                elif intencao in 'tabela':
+                    tabela.append(frase)
+
+                elif intencao in 'pedido':
+                    pedido.append(frase)
+
+                elif intencao in 'conta':
+                    conta.append(frase)
+
+                elif intencao in 'confirmacao':
+                    confirmacao.append(frase)
+
+                elif intencao in 'negacao':
+                    negacao.append(frase)
+
+                elif intencao in 'agradecimento':
+                    agradecimento.append(frase)
+
+                elif intencao in 'continuidade':
+                    continuidade.append(frase)
+
+        a = dict()
+        a['saudacao'] = saudacao
+        a['tabela'] = tabela
+        a['pedido'] = pedido
+        a['conta'] = conta
+        a['confirmacao'] = confirmacao
+        a['negacao'] = negacao
+        a['agradecimento'] = agradecimento
+        a['continuidade'] = continuidade
+
+        return a
+
+    def escolherResposta(self, intencao):
+        if intencao in self.listaRespostas.keys():
+            return choice(self.listaRespostas[intencao])
+
+        else:
+            return 'Em que posso ser útil?'
+
+    def showMenu(self):
+        for key, value in self.listaProdutosPreco.items():
+            print('{} -> R$ {:0.2f}'.format(key, float(value)))
+
+    def adicionarCarrinho(self, listaProduto, listaQuantidade):
+        for index in range(len(listaProduto)):
+            preco = self.listaProdutosPreco.get(listaProduto[index])
+            p = dict()
+            p[listaProduto[index]] = [listaQuantidade[index], listaQuantidade[index] * preco]
+            self.cart.append(p)
+
+    def saida(self):
+        # TODO: Fechar carrinho se tiver produtos
+        pass
+
+    def validarPedido(self, frase):
+        palavras = frase.split(' ')
+        quantidade = None
+        produto = None
+        multiplicador = 1
+
+        lista_quantidades = list()
+        lista_produtos = list()
+        index = 0
+        for palavra in palavras:
+            if self.listaPacotes.get(palavra):
+                multiplicador = self.listaPacotes.get(palavra)
+
+            if quantidade is None:
+                quantidade = self.listaNumeros.get(palavra)
+
+            if produto is None:
+                produto = self.listaProdutos.get(palavra)
+
+            if palavra.isdigit():
+                quantidade = palavra
+
+            quantidade = 1 if quantidade is None else quantidade
+
+            if produto is not None:
+                lista_quantidades.append(int(quantidade) * multiplicador)
+                lista_produtos.append(produto)
+
+                index += 1
+                quantidade = None
+                produto = None
+                multiplicador = 1
+
+        self.adicionarCarrinho(lista_produtos, lista_quantidades)
+
+    def buildProductList(self):
+        p = dict()
+        with open('../files/produtos.csv', 'r', encoding = 'utf-8-sig') as file:
+            for line in file:
+                chave, valor = line.replace('\n', '').replace(' ', '').split(';')
+                p[chave] = valor
+
+        return p
+
+    def buildNumberList(self):
+        n = dict()
+        with open('../files/numeros.csv', mode = 'r', encoding = 'utf-8-sig') as file:
+            for line in file:
+                str, num = line.replace('\n', '').split(';')
+                n[str] = int(num)
+
+        return n
+
+    def buildProductsPrice(self):
+        p = dict()
+        with open('../files/precos.csv', 'r', encoding = 'utf-8-sig') as file:
+            for line in file:
+                chave, valor = line.replace('\n', '').split(';')
+                p[chave] = float(valor)
+
+        return p
+
+    def buildPackages(self):
+        p = dict()
+        with open('../files/pacotes.csv', 'r', encoding = 'utf-8-sig') as file:
+            for line in file:
+                str, num = line.replace('\n', '').split(';')
+                p[str] = int(num)
+
+        return p
+
+    def fecharConta(self):
+        if len(self.cart) > 0:
+            print('{h} Pedido número {p} {h}'.format(h = '=' * 8, p = choice(range(1000, 9999))))
+
+            index = 1
+            total = 0
+            for item in self.cart:
+                for key, value in item.items():
+                    print('{}. {} \t{}\t{:.2f}'.format(index, key, value[0], value[1]))
+                    index += 1
+                    total += value[1]
+            print('-' * 36)
+            print('Total.\t\t\t{:.2f}'.format(total))
+            print('=' * 36)
 
     def setClassificador(self, x):
         if x in 'tree':
@@ -39,13 +205,13 @@ class Bot:
     def fit(self, arquivo):
         with open(arquivo, 'r', encoding = 'utf-8-sig') as file:
             for line in file:
-                s = line.replace("\n", "").split('-')
+                s = line.replace('\n', '').split(';')
                 self.intencoes.append(s[0])
                 self.dialogos.append(s[1])
 
         with open('../files/produtos.csv', 'r', encoding = 'utf-8-sig') as file:
             for line in file:
-                s = line.replace("\n", "").split(';')
+                s = line.replace('\n', '').split(';')
                 self.intencoes.append('pedido')
                 self.dialogos.append(s[0])
 
--- main/main.py
+++ main/main.py
@@ -1,134 +1,11 @@
 __author__ = 'Marco Almeida and André Furlan'
 
 from bot.bot import Bot
-import random
-
-produtos = dict()
-produtosPrecos = dict()
-carrinho = list()
-numeros = dict()
-frases_saudacao = list()
-frases_tabela = list()
-frases_pedido = list()
-frases_conta = list()
-frases_confirmacao = list()
-frases_negacao = list()
-frases_agradecimento = list()
-pacotes = dict()
-
-def controiPacotes():
-    with open('../files/pacotes.csv', 'r', encoding = 'utf-8-sig') as file:
-        for line in file:
-            str, num = line.replace('\n', '').split(';')
-            pacotes[str] = int(num)
-
-def constroiRespostas():
-    with open('../files/respostas.csv', 'r', encoding = 'utf-8-sig') as file:
-        for line in file:
-            intencao = line.replace('\n', '').split(';')[0]
-            frase = line.replace('\n', '').split(';')[1]
-            if intencao == 'saudacao':
-                frases_saudacao.append(frase)
-            elif intencao == 'tabela':
-                frases_tabela.append(frase)
-            elif intencao == 'pedido':
-                frases_pedido.append(frase)
-            elif intencao == 'conta':
-                frases_conta.append(frase)
-            elif intencao == 'confirmacao':
-                frases_confirmacao.append(frase)
-            elif intencao == 'negacao':
-                frases_negacao.append(frase)
-            elif intencao == 'agradecimento':
-                frases_agradecimento.append(frase)
-
-def constroiNumeros():
-    with open('../files/numeros.csv', mode = 'r', encoding = 'utf-8-sig') as file:
-        for line in file:
-            str, num = line.replace('\n', '').split(';')
-            numeros[str] = int(num)
-
-def somaConta(conta):
-    #TODO
-    pass
-
-def exibeProdutos():
-    print('\n' + random.choice(frases_saudacao))
-    for key, value in produtos.items():
-        print('{} -> R${}'.format(key, value).replace('\n', ''))
-
-def cardapio():
-    with open('../files/precos.csv', 'r', encoding = 'utf-8-sig') as file:
-        for line in file:
-            produto, valor = line.replace('\n', '').split(';')
-            produtosPrecos[produto] = valor
-
-def controiProdutos():
-    with open('../files/produtos.csv', 'r', encoding = 'utf-8-sig') as file:
-        for line in file:
-            chave, valor = line.replace('\n', '').replace(' ', '').split(';')
-            produtos[chave] = valor
-
-def adicionaCarrinho(lista_produto, lista_quantidade):
-    index = 0
-    #FIXME Corregir multiplicacao de pacotes
-    for index in range(len(lista_produto)):
-        preco = produtosPrecos.get(lista_produto[index])
-        print(preco)
-        p = {'{}:{}:{}'.format(lista_produto[index], lista_quantidade[index], lista_quantidade[index] * preco )}
-        carrinho.append(p)
-
-    print(carrinho)
-    print(random.choice(frases_pedido))
-
-def saida():
-    if len(produtos) > 0:
-        somaConta(carrinho)
-        print('sua conta total eh de xxxx')
-
-    print('\n'+ random.choice(frases_agradecimento))
-
-    return False
-
-
-def verificarPedido(entrada):
-    palavras = entrada.split(' ')
-    quantidade = None
-    produto = None
-    multiplicador = 1
-    print(palavras)
-
-    lista_quantidades = list()
-    lista_produtos = list()
-    index = 0
-    for palavra in palavras:
-        if pacotes.get(palavra): multiplicador = pacotes.get(palavra)
-        if quantidade is None: quantidade = numeros.get(palavra)
-        if produto is None: produto = produtos.get(palavra)
-        if palavra.isdigit(): quantidade = palavra
-        quantidade = 1 if quantidade is None else quantidade
-
-        if produto is not None:
-            lista_quantidades.append(int(quantidade) * multiplicador)
-            lista_produtos.append(produto)
-
-            index += 1
-            quantidade = None
-            produto = None
-            multiplicador = 1
-
-    adicionaCarrinho(lista_produtos, lista_quantidades)
 
 def main():
     bot = Bot('../files/dialogo.csv')
-    constroiNumeros()
-    cardapio()
-    constroiRespostas()
-    controiPacotes()
-    controiProdutos()
-
-    print('{h} Bem vindo a SJN Polpas {h}\n'.format(h = '#'* 8))
-    print(random.choice(frases_saudacao))
+    print('{h} Bem vindo a SJN Polpas {h}\n'.format(h = '#' * 8))
+    print(bot.escolherResposta('saudacao'))
 
     loop = True
 
@@ -136,26 +13,25 @@ def main():
         entrada = input()
         int = bot.predict(entrada)[0]
 
-        print('Intencao {}'.format(int))
-
         if int in 'saudacao':
-            print(random.choice(frases_saudacao))
+            print(bot.escolherResposta(int))
 
         elif int in 'pedido':
-            verificarPedido(entrada)
-            print(carrinho)
+            bot.validarPedido(entrada)
 
         elif int in 'conta':
-            somaConta(carrinho)
+            bot.fecharConta()
 
         elif int in 'tabela':
-            exibeProdutos()
+            print(bot.escolherResposta('tabela'))
+            bot.showMenu()
 
         elif int in 'saida':
-            loop = saida()
+            bot.fecharConta()
+            loop = False
 
         if loop is True:
-            print('Deseja algo mais?')
+            print('\n{}'.format(bot.escolherResposta('continuidade')))
 
 if __name__ == '__main__':
     main()
